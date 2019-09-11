3d34eb349be432e3ae5780b09bad8e2171fcb23d
Joost Baptist <joostbaptist@gmail.com>
2019-09-11 14:05:03 +0200

    Added exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,20 @@
 export { default as useForm } from './useForm';
 export { default as FormManager } from './FormManager';
 export * from './InputHandlers';
+
+export {
+    FormState,
+    InputOptions,
+    CheckboxOptions,
+    RadioOptions,
+    ChecklistOptions,
+    TextareaOptions,
+    SelectOptions,
+    InputProps,
+    CheckboxProps,
+    RadioProps,
+    ChecklistProps,
+    TextareaProps,
+    SelectProps,
+    OptionProps
+} from './FormManager';
